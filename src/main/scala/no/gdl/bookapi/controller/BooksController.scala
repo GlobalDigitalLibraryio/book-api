7aff8d30ab40020bfdf81ed40e4a57c617239e31
Kenneth Stigen <kes@knowit.no>
2017-09-15 09:45:59 +0200

    #17: Updated swagger documentation

--- src/main/scala/no/gdl/bookapi/controller/BooksController.scala
+++ src/main/scala/no/gdl/bookapi/controller/BooksController.scala
@@ -31,68 +31,83 @@ trait BooksController {
     val response404 = ResponseMessage(404, "Not found", Some("Error"))
     val response500 = ResponseMessage(500, "Unknown error", Some("Error"))
 
-    val getAllBooksDoc =
-      (apiOperation[Seq[api.SearchResult]]("getAllBooks")
-        summary s"Returns all books in the default language $DefaultLanguage"
-        notes s"Returns a list of books in $DefaultLanguage"
-        parameters(
-          headerParam[Option[String]]("X-Correlation-ID").description("User supplied correlation-id. May be omitted."),
-          queryParam[Option[Int]]("page-size").description("Return this many results per page"),
-          queryParam[Option[Int]]("page").description("Return results for this page")
-        )
-        authorizations "oauth2"
-        responseMessages(response500))
+    private val getAllBooks = (apiOperation[api.SearchResult]("getAllBooks")
+      summary s"Returns all books in the default language $DefaultLanguage"
+      notes s"Returns a list of books in $DefaultLanguage"
+      parameters(
+      headerParam[Option[String]]("X-Correlation-ID").description("User supplied correlation-id. May be omitted."),
+      queryParam[Option[Int]]("page-size").description("Return this many results per page."),
+      queryParam[Option[Int]]("page").description("Return results for this page."))
+      authorizations "oauth2"
+      responseMessages response500)
 
-    val getAllBooksInLangDoc =
-      (apiOperation[Seq[api.SearchResult]]("getAllBooksInLangDoc")
-        summary s"Returns all books in the default language $DefaultLanguage"
-        notes s"Returns a list of books in $DefaultLanguage"
-        parameters(
-        headerParam[Option[String]]("X-Correlation-ID").description("User supplied correlation-id. May be omitted."),
-        queryParam[Option[Int]]("page-size").description("Return this many results per page"),
-        queryParam[Option[Int]]("page").description("Return results for this page")
-      )
-        authorizations "oauth2"
-        responseMessages(response500))
+    private val getAllBooksInLang = (apiOperation[api.SearchResult]("getAllBooksInLangDoc")
+      summary s"Returns all books in the specified language"
+      notes s"Returns a list of books in the specified language"
+      parameters(
+      headerParam[Option[String]]("X-Correlation-ID").description("User supplied correlation-id. May be omitted."),
+      pathParam[Long]("lang").description("Desired language for books specified in ISO 639-2 format."),
+      queryParam[Option[Int]]("page-size").description("Return this many results per page."),
+      queryParam[Option[Int]]("page").description("Return results for this page."))
+      authorizations "oauth2"
+      responseMessages response500)
 
-    val getBookDoc =
-      (apiOperation[String]("getBook")
-        summary "Returns metadata about a book"
-        notes "Returns a book"
-        parameters(
-          pathParam[Long]("id").description("Id of the book that is to be returned"),
-          queryParam[Option[String]]("language").description(s"Return the book on this language. Default is $DefaultLanguage")
-        )
-        authorizations "oauth2"
-        responseMessages(response400, response404, response500))
+    private val getBook = (apiOperation[Option[api.Book]]("getBookInLanguageDoc")
+      summary "Returns metadata about a book for the given language"
+      notes "Returns a book in the given language"
+      parameters(
+      headerParam[Option[String]]("X-Correlation-ID").description("User supplied correlation-id. May be omitted."),
+      pathParam[String]("lang").description("Desired language for books specified in ISO 639-2 format"),
+      pathParam[Long]("id").description("Id of the book that is to be returned."))
+      authorizations "oauth2"
+      responseMessages(response400, response404, response500))
 
-    val getBookInLanguageDoc =
-      (apiOperation[String]("getBook")
-        summary "Returns metadata about a book for the given language"
-        notes "Returns a book in the given language"
-        parameters(
-          pathParam[Long]("id").description("Id of the book that is to be returned"),
-          pathParam[String]("language").description("ISO 639-2 language code")
-        )
-        authorizations "oauth2"
-        responseMessages(response400, response404, response500))
+    private val getChapters = (apiOperation[Seq[api.ChapterSummary]]("getChapters")
+      summary "Returns metadata about the chapters for a given book in the given language"
+      notes "Returns metadata about the chapters for a given book in the given language"
+      parameters(
+      headerParam[Option[String]]("X-Correlation-ID").description("User supplied correlation-id. May be omitted."),
+      pathParam[String]("lang").description("Desired language for books specified in ISO 639-2 format"),
+      pathParam[Long]("id").description("Id of the book that is to be returned."))
+      authorizations "oauth2"
+      responseMessages(response400, response404, response500))
 
+    private val getChapter = (apiOperation[Option[api.Chapter]]("getChapter")
+      summary "Returns metadata and content for the given chapter in the given language for the given book."
+      notes "Returns metadata and content for the given chapter in the given language for the given book."
+      parameters(
+      headerParam[Option[String]]("X-Correlation-ID").description("User supplied correlation-id. May be omitted."),
+      pathParam[String]("lang").description("Desired language for books specified in ISO 639-2 format"),
+      pathParam[Long]("bookid").description("Id of the book that is to be returned."),
+      pathParam[Long]("chapterid").description("Id of the chapter that is to be returned."))
+      authorizations "oauth2"
+      responseMessages(response400, response404, response500))
 
-    get("/", operation(getAllBooksDoc)) {
+    private val getSimilar = (apiOperation[api.SearchResult]("getSimilar")
+      summary "Returns books that are similar to the given id in the given language."
+      notes "Returns books that are similar to the given id in the given language."
+      parameters(
+      headerParam[Option[String]]("X-Correlation-ID").description("User supplied correlation-id. May be omitted."),
+      pathParam[String]("lang").description("Desired language for books specified in ISO 639-2 format"),
+      pathParam[Long]("id").description("Id of the book to find similar for."))
+      authorizations "oauth2"
+      responseMessages(response400, response404, response500))
+
+    get("/", operation(getAllBooks)) {
       val pageSize = intOrDefault("page-size", 10).min(100).max(1)
       val page = intOrDefault("page", 1).max(0)
 
       readService.withLanguage(DefaultLanguage, pageSize, page)
     }
 
-    get("/:lang/?", operation(getAllBooksInLangDoc) ) {
+    get("/:lang/?", operation(getAllBooksInLang)) {
       val pageSize = intOrDefault("page-size", 10).min(100).max(1)
       val page = intOrDefault("page", 1).max(0)
 
       readService.withLanguage(params("lang"), pageSize, page)
     }
 
-    get("/:lang/:id/?", operation(getBookInLanguageDoc)) {
+    get("/:lang/:id/?", operation(getBook)) {
       val id = long("id")
       val language = params("lang")
 
@@ -102,14 +117,14 @@ trait BooksController {
       }
     }
 
-    get("/:lang/:id/chapters/?") {
+    get("/:lang/:id/chapters/?", operation(getChapters)) {
       val language = params("lang")
       val id = long("id")
 
       readService.chaptersForIdAndLanguage(id, language)
     }
 
-    get("/:lang/:bookid/chapters/:chapterid/?") {
+    get("/:lang/:bookid/chapters/:chapterid/?", operation(getChapter)) {
       val language = params("lang")
       val bookId = long("bookid")
       val chapterId = long("chapterid")
@@ -118,10 +133,9 @@ trait BooksController {
         case Some(x) => x
         case None => NotFound(body = Error(Error.NOT_FOUND, s"No chapter with id $chapterId for book with id $bookId and language $language found."))
       }
-
     }
 
-    get("/:lang/similar/:id/?") {
+    get("/:lang/similar/:id/?", operation(getSimilar)) {
       val pageSize = intOrDefault("page-size", 10).min(100).max(1)
       val page = intOrDefault("page", 1).max(0)
 
