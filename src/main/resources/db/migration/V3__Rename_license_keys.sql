032b63e2dd6894e37d2bc006077f19af72f0b127
Kenneth Stigen <kes@knowit.no>
2017-09-19 09:53:28 +0200

    #17: Renamed license keys

---
+++ src/main/resources/db/migration/V3__Rename_license_keys.sql
@@ -0,0 +1,3 @@
+update license set NAME = 'cc-by-4.0' where name = 'cc by 4.0';
+update license set NAME = 'cc-by-sa-4.0' where name = 'cc by-sa 4.0';
+update license set NAME = 'cc-by-nd-4.0' where name = 'cc by-nd 4.0';
