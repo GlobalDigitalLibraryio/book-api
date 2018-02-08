de0fac91f0cb3307ae845bdc6299d63434a19b9c
Runar Furenes <ruf@knowit.no>
2018-02-08 10:00:19 +0100

    #94: This can be done now GlobalDigitalLibraryio/issues#94

--- src/main/scala/no/gdl/bookapi/repository/FeedRepository.scala
+++ src/main/scala/no/gdl/bookapi/repository/FeedRepository.scala
@@ -26,7 +26,7 @@ trait FeedRepository {
         .map(Feed(f)).single().apply()
     }
 
-    // TODO Change this to "upsert" (on conflict do update) when we're on PostgreSQL >= 9.5
+    // TODO Change this to "upsert" (on conflict do update), which exists in PostgreSQL >= 9.5
     // https://www.postgresql.org/docs/devel/static/sql-insert.html
     def addOrUpdate(feed: Feed)(implicit session: DBSession = AutoSession): Feed = {
       val f = Feed.column
