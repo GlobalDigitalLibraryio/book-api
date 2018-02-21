488b5d6b3d12984dc9b8537221c58b463131976b
Gunnar Velle <gunnar.velle@gmail.com>
2018-02-21 11:28:01 +0100

    #231 nullpointer when highlight is null

--- src/main/scala/no/gdl/bookapi/service/search/SearchService.scala
+++ src/main/scala/no/gdl/bookapi/service/search/SearchService.scala
@@ -113,12 +113,16 @@ trait SearchService {
       val element = json match {
         case book: Book => book
         case bookHit: BookHit =>
-          (hit.highlight.get("title"), hit.highlight.get("description")) match {
-            case (None, None) => bookHit
-            case (Some(title), None) => bookHit.copy(highlightTitle = Some(title.head))
-            case (None, Some(description)) => bookHit.copy(highlightDescription = Some(description.head))
-            case (Some(title), Some(description)) => bookHit.copy(
-              highlightTitle = Some(title.head), highlightDescription = Some(description.head))
+          if(hit.highlight == null) {
+            bookHit
+          } else {
+            (hit.highlight.get("title"), hit.highlight.get("description")) match {
+              case (None, None) => bookHit
+              case (Some(title), None) => bookHit.copy(highlightTitle = Some(title.head))
+              case (None, Some(description)) => bookHit.copy(highlightDescription = Some(description.head))
+              case (Some(title), Some(description)) => bookHit.copy(
+                highlightTitle = Some(title.head), highlightDescription = Some(description.head))
+            }
           }
       }
       element.asInstanceOf[B]
