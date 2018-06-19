dd4d20e580abceb22914d3f46232f460d1aecbea
Runar Furenes <ruf@knowit.no>
2018-06-19 13:42:40 +0200

    #389: Delete redundant chapters on update book GlobalDigitalLibraryio/issues#389

--- src/main/scala/io/digitallibrary/bookapi/repository/ChapterRepository.scala
+++ src/main/scala/io/digitallibrary/bookapi/repository/ChapterRepository.scala
@@ -97,5 +97,10 @@ trait ChapterRepository {
         .eq(ch.seqNo, seqno).toSQL
         .map(Chapter(ch)).single().apply()
     }
+
+    def deleteChaptersExceptGivenSeqNumbers(translationId: Long, seqNumbersToKeep: Seq[Int])(implicit session: DBSession = AutoSession): Unit = {
+      val ch = Chapter.column
+      deleteFrom(Chapter).where.notIn(ch.seqNo, seqNumbersToKeep).toSQL.update().apply()
+    }
   }
 }
